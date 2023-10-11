b008e60bc20275fb66954f707b54119902911a61
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2023-10-11 17:03:29 +0100

    Initial import

---
+++ defaultCode/stochastic.R
@@ -0,0 +1,27 @@
+# parameters
+N <- user(1e6)
+I_init <- user(1)
+beta <- user(4)
+d <- user(0.5)
+
+# The step size, in units of time
+dt <- 0.01
+
+# Stochastic solution
+initial(S) <- N - I_init
+initial(I) <- I_init
+initial(R) <- 0
+
+FOI <- beta * I / N
+
+n_S_to_I <- rbinom(S, FOI * dt)
+n_I_to_R <- rbinom(I, (1/d) * dt)
+
+# update for next time step
+update(S) <- S - n_S_to_I
+update(I) <- I + n_S_to_I - n_I_to_R
+update(R) <- R + n_I_to_R
+
+# to monitor if the epidemic goes extinct
+initial(extinct) <- I == 0
+update(extinct) <- I == 0
